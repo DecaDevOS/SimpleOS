91f833dd3e4c1dd9710fd3e4bf8b2e48f5edc73f
Tom Tarpey <tomtarpeydev@gmail.com>
2020-01-23 07:08:19 -0500

    (kmem.h): Mem set

--- src/include/kmem.h
+++ src/include/kmem.h
@@ -4,3 +4,7 @@
 /* (memcpy): Copy from source to destination. Assumes that sorce and destination are not overlapping */
 extern void *memcpy(void * restrict dest, const void * restrict src, size_t count);
 
+/* (memset): Set count amount of bytes to value */
+extern void *memset(void *dest, int val, size_t count);
+
+
