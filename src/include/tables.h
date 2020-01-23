f2e7119931ec54b7a1ca664c7954c6dade20dc08
Tom Tarpey <tomtarpeydev@gmail.com>
2020-01-23 11:14:43 -0500

    (tables.h): Create register Structure

--- src/include/tables.h
+++ src/include/tables.h
@@ -13,4 +13,30 @@ extern void idt_install();
 /* (idt_set_gate): Set the IDT Descriptor */
 extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flag);
 
+/* Registers Structure */
+struct registers {
+	unsigned int gs;
+	unsigned int fs;
+	unsigned int es;
+	unsigned int ds;
+
+	unsigned int edi;
+	unsigned int esi;
+	unsigned int ebp;
+	unsigned int esp;
+	unsigned int ebx;
+	unsigned int edx;
+	unsigned int ecx;
+	unsigned int eax;
+
+	unsigned int int_no;
+	unsigned int err_code;
+
+	unsigned int eip;
+	unsigned int cs;
+	unsigned int eflags;
+	unsigned int useresp;
+	unsigned int ss;
+};
+
 
