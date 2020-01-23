29ba7a325df5276a9db8b55486dfa523a1e83901
Tom Tarpey <tomtarpeydev@gmail.com>
2020-01-23 10:56:48 -0500

    (tables.h): (GDT): Set Gate

--- src/include/tables.h
+++ src/include/tables.h
@@ -3,3 +3,8 @@
 
 /* (gdt_install): Install the kernels GDTs */
 extern void gdt_install();
+
+/* (gdt_set_gate): Set the GDT Descriptor */
+extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
+
+
