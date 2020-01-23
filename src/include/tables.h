3b2cc8e4575aa6dab0c29491e981617eb6f1c109
Tom Tarpey <tomtarpeydev@gmail.com>
2020-01-23 10:59:40 -0500

    (tables.h): (IDT): Install

--- src/include/tables.h
+++ src/include/tables.h
@@ -7,4 +7,6 @@ extern void gdt_install();
 /* (gdt_set_gate): Set the GDT Descriptor */
 extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
 
+/* (idt_install): Install the kernels IDTs */
+extern void idt_install();
 
