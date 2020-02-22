04fd4adb2e7496dcfb72c50f40135bb6162cd887
Tom <tomtarpeydev@gmail.com>
2020-02-22 11:56:58 +0000

    (core/idt.c): idt entry struct

--- src/core/idt.c
+++ src/core/idt.c
@@ -1,1 +1,12 @@
 /* Interrupt Descriptor Table */
+# include <ksys.h>
+
+/* IDT Entry */
+struct idt_entry {
+    unsigned short base_low;
+    unsigned short sel;
+    unsigned char zero;
+    unsigned char flags;
+    unsigned short base_high;
+} __attribute__((packed));
+
