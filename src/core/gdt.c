dfde8295dc92bc767cc4702545466987478b1d2e
Tom <tomtarpeydev@gmail.com>
2020-02-22 11:04:58 +0000

    (core/gdt.c): setup an external gdt set gate

--- src/core/gdt.c
+++ src/core/gdt.c
@@ -21,3 +21,22 @@ struct gdt_entry gdt[3];
 struct gdt_ptr   gp;
 
 extern void gdt_flush();
+
+/* gdt_set_gate: Set a GDT descriptor */
+extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) {
+    /* Base Address */
+    gdt[num].base_low = (base & 0xFFFF);
+    gdt[num].base_middle = (base >> 16) & 0xFF;
+    gdt[num].base_high = (base >> 24) & 0xFF;
+
+    /* Limits */
+    gdt[num].limit_low = (limit & 0xFFFF);
+    gdt[num].granularity = (limit >> 16) & 0x0F;
+
+    /* Granularity */
+    gdt[num].granularity |= (gran & 0xF0);
+
+    /* Access Flags */
+    gdt[num].access = access;
+}
+
