53af4d43b980264bd6aa5af7fefc2d9eec8ee9d7
Tom <tomtarpeydev@gmail.com>
2020-02-22 12:13:39 +0000

    (core/idt.c): idt install

--- src/core/idt.c
+++ src/core/idt.c
@@ -21,10 +21,20 @@ struct idt_ptr idtp;
 
 extern void idt_load();
 
+/* IDT Set Gate */
 extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
     idt[num].base_low = (base & 0xFFFF);
     idt[num].base_high = (base >> 16) & 0xFFFF;
     idt[num].sel = sel;
     idt[num].zero = 0;
     idt[num].flags = flags;
+}
+
+/* IDT Install */
+void idt_install() {
+    idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+    idtp.base = &idt;
+    memset(&idt, 0, sizeof(struct idt_entry) * 256));
+
+    idt_load();
 }
