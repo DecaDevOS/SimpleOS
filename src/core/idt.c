0cb3ddc46fda514f546b7b6525a22eb455ed4710
Tom <tomtarpeydev@gmail.com>
2020-02-22 12:09:26 +0000

    (core/idt.c): idt set gate

--- src/core/idt.c
+++ src/core/idt.c
@@ -20,3 +20,11 @@ struct idt_entry idt[256];
 struct idt_ptr idtp;
 
 extern void idt_load();
+
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
+    idt[num].base_low = (base & 0xFFFF);
+    idt[num].base_high = (base >> 16) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].zero = 0;
+    idt[num].flags = flags;
+}
