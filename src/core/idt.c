bcb680d2ab1bf9a162e50a32e9c62106a6a99fd4
Tom <tomtarpeydev@gmail.com>
2020-02-22 12:00:13 +0000

    (core/idt.c): idt pointer struct

--- src/core/idt.c
+++ src/core/idt.c
@@ -10,3 +10,9 @@ struct idt_entry {
     unsigned short base_high;
 } __attribute__((packed));
 
+/* IDT Pointer */
+struct idt_ptr {
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
